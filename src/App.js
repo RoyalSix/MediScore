3aafe940f66c686d83674e8a98de399b7d7e2616
RoyalSix <jay.a.scottjr@biola.edu>
2017-05-13 15:40:45 -0700

    added map to webpage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as map from './map.js';
 
 class App extends Component {
+  componentDidMount(){
+    map.initMap();
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div style={{display:'flex', justifyContent:'flex-end'}}>
+        <div id="map" style={{backgroundColor:'black', height:500, width:500, margin:50}}>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+//lat long score name
